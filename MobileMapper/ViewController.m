ab25b4e7f1d02e642b7b487b0298fb1a23f31595
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-14 13:07:42 -0500

    hackwich

--- MobileMapper/ViewController.m
+++ MobileMapper/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property MKPointAnnotation *mobileMakersAnnotation;
+@property CLLocationManager *locationManager;
 
 @end
 
@@ -16,12 +20,57 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.locationManager = [[CLLocationManager alloc]init];
+    [self.locationManager requestWhenInUseAuthorization];
+
+    CLLocationCoordinate2D coord;
+    coord.latitude = 41.89373984;
+    coord.longitude = -87.63532979;
+
+    self.mobileMakersAnnotation = [[MKPointAnnotation alloc] init];
+    self.mobileMakersAnnotation.coordinate = coord;
+    self.mobileMakersAnnotation.title = @"Mobile Makers";
+    [self.mapView addAnnotation:self.mobileMakersAnnotation];
+
+    [self addMountRushmorePin];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)addMountRushmorePin{
+    CLGeocoder *geocoder = [[CLGeocoder alloc]init];
+    [geocoder geocodeAddressString:@"Mount Rushmore" completionHandler:^(NSArray *placemarks, NSError *error) {
+        for (CLPlacemark *placemark in placemarks) {
+            MKPointAnnotation *annotation = [[MKPointAnnotation alloc]init];
+            annotation.coordinate = placemark.location.coordinate;
+            annotation.title = @"Mount Rushmore";
+            [self.mapView addAnnotation:annotation];
+        }
+    }];
+}
+
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    if (annotation == mapView.userLocation) {
+        return nil;
+    }
+
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"MyPinID"];
+    pin.canShowCallout = YES;
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    pin.image = [UIImage imageNamed:@"pinImage"];
+
+    return pin;
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control{
+    CLLocationCoordinate2D center = view.annotation.coordinate;
+    MKCoordinateSpan span;
+    span.latitudeDelta = .01;
+    span.longitudeDelta = .01;
+    MKCoordinateRegion region;
+    region.center = center;
+    region.span = span;
+    [self.mapView setRegion:region animated:YES];
 }
 
 @end
